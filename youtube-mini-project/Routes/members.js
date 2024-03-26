6c8f6744cbba0d1eda64a8cd35a7725f6af76000
jihwan1211 <jhwank1211@gmail.com>
2024-03-26 15:30:29 +0900

    3/26

--- youtube-mini-project/Routes/members.js
+++ youtube-mini-project/Routes/members.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const membersHandlers = require("../Controllers/membersHandlers");
+const membersHandlers = require("../Controllers/members");
 
 router.post("/login", membersHandlers.postLogin);
 router.post("/join", membersHandlers.postJoin);
-router.get("/users/:id", membersHandlers.getUser);
-router.delete("/users/:id", membersHandlers.deleteUser);
+router.get("/users", membersHandlers.getUser);
+router.delete("/users", membersHandlers.deleteUser);
 
 exports.router = router;
